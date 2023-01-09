bbdd753545a6c0b0b8ce07495af54dd0fc414aff
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-01-09 23:10:17 +0900

    FEAT : Query Dsl 구현

--- src/main/java/com/example/pirate99_final/map/service/NaverService.java
+++ src/main/java/com/example/pirate99_final/map/service/NaverService.java
@@ -2,6 +2,7 @@ package com.example.pirate99_final.map.service;
 
 import com.example.pirate99_final.map.entity.Naver;
 import com.example.pirate99_final.map.repository.NaverRepository;
+import com.example.pirate99_final.map.repository.NaverRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 @RequiredArgsConstructor
 public class NaverService {
     private final NaverRepository naverRepository;
+    private final NaverRepositoryImpl naverRepositoryImpl;
 
     public void naverMap(Model model, String storeName) {
         // 1. DB에서 가게 이름을 조회 한다.
@@ -18,6 +20,32 @@ public class NaverService {
         );
         // 2. 조회한 가게이름의 위도 경도 값을 index.html로 보낸다.
         model.addAttribute("Lat", naver.getLat());
-        model.addAttribute("lng", naver.getLng());
+        model.addAttribute("Lng", naver.getLng());
+//****************************************************************************************************************
+//        // 가게명 일치 여부
+//        Naver findByOneStoreName = naverRepositoryImpl.findByStoreName(storeName);
+//        // 가게명 일부 포함
+//        List<Naver> findByStoreNameInclude = naverRepositoryImpl.findByStoreNameInclude(storeName);
+//        // 도로명주소 포함 여부 (도로명주소의 일부 일치)
+//        List<Naver> findByRoadAddressInclude = naverRepositoryImpl.findByroadAddressInclude(roadNameAddress);
+//        // 업종별 분류
+//        List<Naver> findByBusiness = naverRepositoryImpl.findByBusiness(typeOfBusiness);
+//        // 평점 낮은순 ASC
+//        List<Naver> OrderByStarScoreASC = naverRepositoryImpl.OrderByStarScore(starScore);
+//        // 평점 높은순 DESC
+//        List<Naver> OrderByStarScoreDESC = naverRepositoryImpl.OrderByStarScoreDESC(starScore);
+//        // 리뷰 낮은순 ASC
+//        List<Naver> OrderByReviewASC = naverRepositoryImpl.OrderByReview(reviewCnt);
+//        // 리뷰 높은순 DESC
+//        List<Naver> OrderByReviewDESC = naverRepositoryImpl.OrderByReviewDESC(reviewCnt);
+//        // 평점 4점 이상
+//        List<Naver> BetweenStarScoreHigh = naverRepositoryImpl.BetweenStarScoreHigh(starScore);
+//        // 평점 1점 이하
+//        List<Naver> BetweenStarScoreLow = naverRepositoryImpl.BetweenStarScoreLow(starScore);
+//        // 리뷰 1000개 이상
+//        List<Naver> BetweenReviewHigh = naverRepositoryImpl.BetweenReviewHigh(reviewCnt);
+//        // 리뷰 10개 이하
+//        List<Naver> BetweenReviewLow = naverRepositoryImpl.BetweenReviewLow(reviewCnt);
+//****************************************************************************************************************
     }
 }
