8f8a842b12c33c5da5f21e1dea05dab34df5158e
Lee Sang Hoon <nicet.shlee@gmail.com>
2023-01-11 00:33:57 +0900

    REFACTOR: 코드 리팩토링에 대한 커밋

--- src/main/java/com/example/pirate99_final/map/controller/NaverController.java
+++ src/main/java/com/example/pirate99_final/map/controller/NaverController.java
@@ -11,11 +11,80 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class NaverController {
     private final NaverService naverService;
-
-    @GetMapping("/")
-    public String naverMap(Model model, @RequestParam String storeName) {
+    // 가게명 일치 여부
+    @GetMapping("/api/search")
+    public String findByOneStoreName(Model model, @RequestParam String storeName) {
         // RequestParam을 통해 url에 검색어를 담아서 보낸다.
-        naverService.naverMap(model, storeName);
+        naverService.findByOneStoreName(model, storeName);
+        return "index";
+    }
+    // 가게명 일부 포함
+    @GetMapping("/api/search1")
+    public String findByStoreNameInclude(Model model, @RequestParam String storeNameInclude) {
+        // RequestParam을 통해 url에 검색어를 담아서 보낸다.
+        naverService.findByStoreNameInclude(model, storeNameInclude);
+        return "index";
+    }
+    // 도로명주소 포함 여부 (도로명주소의 일부 일치)
+    @GetMapping("/api/search2")
+    public String findByRoadAddressInclude(Model model, @RequestParam String roadNameAddress) {
+        // RequestParam을 통해 url에 검색어를 담아서 보낸다.
+        naverService.findByRoadAddressInclude(model, roadNameAddress);
+        return "index";
+    }
+    // 업종별 분류
+    @GetMapping("/api/search3")
+    public String findByBusiness(Model model, @RequestParam String typeOfBusiness) {
+        // RequestParam을 통해 url에 검색어를 담아서 보낸다.
+        naverService.findByBusiness(model, typeOfBusiness);
+        return "index";
+    }
+    // 평점 낮은순 ASC
+    @GetMapping("/api/search4")
+    public String OrderByStarScoreASC(Model model) {
+        naverService.OrderByStarScoreASC(model);
+        return "index";
+    }
+    // 평점 높은순 DESC
+    @GetMapping("/api/search5")
+    public String OrderByStarScoreDESC(Model model) {
+        naverService.OrderByStarScoreDESC(model);
+        return "index";
+    }
+    // 리뷰 낮은순 ASC
+    @GetMapping("/api/search6")
+    public String OrderByReviewASC(Model model) {
+        naverService.OrderByReviewASC(model);
+        return "index";
+    }
+    // 리뷰 높은순 DESC
+    @GetMapping("/api/search7")
+    public String OrderByReviewDESC(Model model) {
+        naverService.OrderByReviewDESC(model);
+        return "index";
+    }
+    // 평점 4점 이상
+    @GetMapping("/api/search8")
+    public String BetweenStarScoreHigh(Model model) {
+        naverService.BetweenStarScoreHigh(model);
+        return "index";
+    }
+    // 평점 2점 이하
+    @GetMapping("/api/search9")
+    public String BetweenStarScoreLow(Model model) {
+        naverService.BetweenStarScoreLow(model);
+        return "index";
+    }
+    // 리뷰 1000개 이상
+    @GetMapping("/api/search10")
+    public String BetweenReviewHigh(Model model) {
+        naverService.BetweenReviewHigh(model);
+        return "index";
+    }
+    // 리뷰 10개 이하
+    @GetMapping("/api/search11")
+    public String BetweenReviewLow(Model model) {
+        naverService.BetweenReviewLow(model);
         return "index";
     }
 }
--- src/main/java/com/example/pirate99_final/map/service/NaverService.java
+++ src/main/java/com/example/pirate99_final/map/service/NaverService.java
@@ -7,45 +7,73 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NaverService {
     private final NaverRepository naverRepository;
     private final NaverRepositoryImpl naverRepositoryImpl;
 
-    public void naverMap(Model model, String storeName) {
-        // 1. DB에서 가게 이름을 조회 한다.
-        Naver naver = naverRepository.findByStoreName(storeName).orElseThrow(
-                () -> new RuntimeException("가게가 존재하지 않습니다.")
-        );
-        // 2. 조회한 가게이름의 위도 경도 값을 index.html로 보낸다.
-        model.addAttribute("Lat", naver.getLat());
-        model.addAttribute("Lng", naver.getLng());
-//****************************************************************************************************************
-//        // 가게명 일치 여부
-//        Naver findByOneStoreName = naverRepositoryImpl.findByStoreName(storeName);
-//        // 가게명 일부 포함
-//        List<Naver> findByStoreNameInclude = naverRepositoryImpl.findByStoreNameInclude(storeName);
-//        // 도로명주소 포함 여부 (도로명주소의 일부 일치)
-//        List<Naver> findByRoadAddressInclude = naverRepositoryImpl.findByroadAddressInclude(roadNameAddress);
-//        // 업종별 분류
-//        List<Naver> findByBusiness = naverRepositoryImpl.findByBusiness(typeOfBusiness);
-//        // 평점 낮은순 ASC
-//        List<Naver> OrderByStarScoreASC = naverRepositoryImpl.OrderByStarScore(starScore);
-//        // 평점 높은순 DESC
-//        List<Naver> OrderByStarScoreDESC = naverRepositoryImpl.OrderByStarScoreDESC(starScore);
-//        // 리뷰 낮은순 ASC
-//        List<Naver> OrderByReviewASC = naverRepositoryImpl.OrderByReview(reviewCnt);
-//        // 리뷰 높은순 DESC
-//        List<Naver> OrderByReviewDESC = naverRepositoryImpl.OrderByReviewDESC(reviewCnt);
-//        // 평점 4점 이상
-//        List<Naver> BetweenStarScoreHigh = naverRepositoryImpl.BetweenStarScoreHigh(starScore);
-//        // 평점 1점 이하
-//        List<Naver> BetweenStarScoreLow = naverRepositoryImpl.BetweenStarScoreLow(starScore);
-//        // 리뷰 1000개 이상
-//        List<Naver> BetweenReviewHigh = naverRepositoryImpl.BetweenReviewHigh(reviewCnt);
-//        // 리뷰 10개 이하
-//        List<Naver> BetweenReviewLow = naverRepositoryImpl.BetweenReviewLow(reviewCnt);
-//****************************************************************************************************************
+    // 가게명 일치 여부
+    public void findByOneStoreName(Model model, String storeName) {
+        Naver findByOneStoreName = naverRepositoryImpl.findByStoreName(storeName);
+        model.addAttribute("Lat", findByOneStoreName.getLat());
+        model.addAttribute("Lng", findByOneStoreName.getLng());
+    }
+    // 가게명 일부 포함
+    public void findByStoreNameInclude(Model model, String storeNameInclude) {
+        List<Naver> findByStoreNameInclude = naverRepositoryImpl.findByStoreNameInclude(storeNameInclude);
+        model.addAttribute("searchList", findByStoreNameInclude);
+    }
+    // 도로명주소 포함 여부 (도로명주소의 일부 일치)
+    public void findByRoadAddressInclude(Model model, String roadNameAddress) {
+        List<Naver> findByRoadAddressInclude = naverRepositoryImpl.findByroadAddressInclude(roadNameAddress);
+        model.addAttribute("searchList", findByRoadAddressInclude);
+    }
+    // 업종별 분류
+    public void findByBusiness(Model model, String typeOfBusiness) {
+        List<Naver> findByBusiness = naverRepositoryImpl.findByBusiness(typeOfBusiness);
+        model.addAttribute("searchList", findByBusiness);
+    }
+    // 평점 낮은순 ASC
+    public void OrderByStarScoreASC(Model model) {
+        List<Naver> OrderByStarScoreASC = naverRepositoryImpl.OrderByStarScore();
+        model.addAttribute("searchList", OrderByStarScoreASC);
+    }
+    // 평점 높은순 DESC
+    public void OrderByStarScoreDESC(Model model) {
+        List<Naver> OrderByStarScoreDESC = naverRepositoryImpl.OrderByStarScoreDESC();
+        model.addAttribute("searchList", OrderByStarScoreDESC);
+    }
+    // 리뷰 낮은순 ASC
+    public void OrderByReviewASC(Model model) {
+        List<Naver> OrderByReviewASC = naverRepositoryImpl.OrderByReview();
+        model.addAttribute("searchList", OrderByReviewASC);
+    }
+    // 리뷰 높은순 DESC
+    public void OrderByReviewDESC(Model model) {
+        List<Naver> OrderByReviewDESC = naverRepositoryImpl.OrderByReviewDESC();
+        model.addAttribute("searchList", OrderByReviewDESC);
+    }
+    // 평점 4점 이상
+    public void BetweenStarScoreHigh(Model model) {
+        List<Naver> BetweenStarScoreHigh = naverRepositoryImpl.BetweenStarScoreHigh();
+        model.addAttribute("searchList", BetweenStarScoreHigh);
+    }
+    // 평점 2점 이하
+    public void BetweenStarScoreLow(Model model) {
+        List<Naver> BetweenStarScoreLow = naverRepositoryImpl.BetweenStarScoreLow();
+        model.addAttribute("searchList", BetweenStarScoreLow);
+    }
+    // 리뷰 1000개 이상
+    public void BetweenReviewHigh(Model model) {
+        List<Naver> BetweenReviewHigh = naverRepositoryImpl.BetweenReviewHigh();
+        model.addAttribute("searchList", BetweenReviewHigh);
+    }
+    // 리뷰 10개 이하
+    public void BetweenReviewLow(Model model) {
+        List<Naver> BetweenReviewLow = naverRepositoryImpl.BetweenReviewLow();
+        model.addAttribute("searchList", BetweenReviewLow);
     }
 }
