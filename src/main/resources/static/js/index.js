ee48da21db7218d8fe0395c4ff012887d4738b13
Kyuu <kyuung09@gmail.com>
2023-01-12 23:21:23 +0900

    FEAT : Waiting Front 기능 구현 #1

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,18 @@
+window.onload=function(){
+    const requestWaiting = document.getElementById('signin');
+    requestWaiting.style.display = 'none';
+
+    const cancleWaiting = document.getElementById('waitingCancle');
+    cancleWaiting.style.display = 'none';
+
+    const statusConfirm = document.getElementById('statusConfirm');
+    statusConfirm.style.display = 'none';
+
+    const myStatusShow = document.getElementById('myStatusShow');
+    myStatusShow.style.display = 'none'
+}
+
+
 function handleSearch() {
     const query = document.querySelector('.form-control').value;
     window.location = "/api/searchLocation?storeName=" + query;
@@ -14,3 +29,44 @@ function onSuccess(position) {
 function onError() {
     alert("위치를 찾을 수 없습니다.");
 }
+
+function waiting() {
+    const requestWaiting = document.getElementById('signin');
+
+    if(requestWaiting.style.display !== 'none') {
+        requestWaiting.style.display = 'none';
+    }
+    else {
+        requestWaiting.style.display = 'block';
+    }
+}
+
+
+
+function cancle() {
+    const cancleWaiting = document.getElementById('waitingCancle');
+
+    if(cancleWaiting.style.display !== 'none') {
+        cancleWaiting.style.display = 'none';
+    }
+    else {
+        cancleWaiting.style.display = 'block';
+    }
+}
+
+function status() {
+    const statusConfirm = document.getElementById('statusConfirm');
+
+    if(statusConfirm.style.display !== 'none') {
+        statusConfirm.style.display = 'none';
+    }
+    else {
+        statusConfirm.style.display = 'block';
+    }
+}
+
+function showStatus(){
+    const myStatusShow = document.getElementById('myStatusShow');
+    myStatusShow.style.display = 'block';
+}
+
