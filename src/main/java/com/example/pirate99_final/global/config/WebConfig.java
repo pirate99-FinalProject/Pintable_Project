0f991678f80005e35eebf475b1391742a1df1f91
Kyuu <kyuung09@gmail.com>
2023-01-13 12:04:15 +0900

    FEAT : CORS Setting #1

---
+++ src/main/java/com/example/pirate99_final/global/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.pirate99_final.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://3.36.123.179")
+                .allowedOrigins("http://3.36.123.179:8080")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
