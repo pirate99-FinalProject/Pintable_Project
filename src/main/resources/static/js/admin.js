cfe62ed58c9db09835cbabe547cf104f5fb94b0f
Kyuu <kyuung09@gmail.com>
2023-02-06 21:37:07 +0900

    [FIX] 손님호출/입장처리 로직 오류 수정

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://pintable.co.kr";
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://pintable.co.kr";
+// const baseUrl = "http://localhost:8080";
 
 window.onload = function () {
     EnterStatus();
